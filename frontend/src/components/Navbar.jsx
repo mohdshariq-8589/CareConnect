c8a8cf5cf1c45f58d68e52978efa54945f8ccf17
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-04-24 11:30:17 +0530

    Small Changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
             <span className="inline-block transition-all duration-300 group-hover:translate-y-[2px] group-hover:scale-[1.05]">
               HOME
             </span>
-            <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-primary transition-all duration-300 group-hover:w-3/5 group-hover:left-[20%]"></span>
+            <span className="absolute left-1/2 bottom-0 w-0 h-0.5 bg-primary transition-all duration-300 group-hover:w-3/5 group-hover:left-[20%] "></span>
           </li>
         </NavLink>
 

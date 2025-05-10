04ae28ef3cc66bfd5aac93640ece5e0b5d196836
Mohd Shariq <155702117+mohdsha-123@users.noreply.github.com>
2025-05-11 00:52:00 +0530

    Update the connection string

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-
-}
+  mongoose.connection.on("connected", () =>
+    console.log("Connection to MongoDB established! Welcome to CareConnect DB.")
+  );
+  await mongoose.connect(`${process.env.MONGODB_URI}`, {dbName:"careConnect"});
+};
 
 export default connectDB;
-
-// Do not use '@' symbol in your databse user's password else it will show an error.
